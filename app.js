cdffb93ca838cb734ce394847b8953446f0bd328
rebaomi <521kakaxi521@163.com>
2014-07-01 16:08:58 +0800

    routes

--- app.js
+++ app.js
@@ -5,9 +5,15 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var MongoStrore = require('connect-mongo')(express);
+var settings = require('./settings');
+var flash = require('connect-flash');
+
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log',{flags:'a'});
+var errorLog = fs.createWriteStream('error.log',{flags:'a'});
 
 var app = express();
 
@@ -15,22 +21,39 @@ var app = express();
 app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(__dirname+"/public/images/favicon.ico"));
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.logger({stream:accessLog}));
+// app.use(express.json());
+// app.use(express.urlencoded());
+app.use(express.bodyParser({keepExtensions:true,uploadDir:'./public/images'}));
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+    secret:settings.cookieSecret,
+    key:settings.db, // cookie name
+    cookie:{maxAge:1000*60*60*24*30},// 30 days
+    store:new MongoStore({
+      db:settings.db
+    })
+}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err,req,res,next){
+  var meta = '[' + new Date() + ']' + req.url + '\n';
+  errorLog.write(meta+err.stack+'\n');
+  next();
+});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app);
